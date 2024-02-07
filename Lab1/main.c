455408bbfdb2bb0047b1b477ce7bf5350cf37965
Tace03 <96904616+Tace03@users.noreply.github.com>
2024-02-07 10:17:13 +0800

    Update main.c

--- Lab1/main.c
+++ Lab1/main.c
@@ -29,3 +29,79 @@ void start_task(void *pvParameters)
     vTaskDelete(StartTask_Handler);
     taskEXIT_CRITICAL();
 }
+
+/* Lab 2 Note
+
+# this is play_button.h
+
+#ifndef __PLAY_BUTTON_H
+#define __PLAY_BUTTON_H
+#include "sys.h"
+#include "system.h"
+
+#define PLAY_BUTTON_TASK_PRIO		3     //Task priority 
+#define PLAY_BUTTON_STK_SIZE 		128   //Task stack size 
+
+*/
+
+
+/*--------Play Button control pin--------*/
+/*
+#define PLAY_BUTTON_PORT GPIOD
+#define PLAY_BUTTON_PIN GPIO_Pin_8
+#define PLAY_BUTTON PDin(8) 
+*/
+/*----------------------------------*/
+/*
+void Play_Button_Init(void);  
+void pbutton_task(void *pvParameters);
+extern int pbutton_Count;
+#endif
+*/
+
+
+
+/*
+
+# play_button.c
+
+#include "play_button.h"
+#include "show.h"
+
+int pbutton_Count=5000;
+
+//LED initialization
+void Play_Button_Init(void)
+{
+	GPIO_InitTypeDef  GPIO_InitStructure;
+	
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);    //Enable APB Clock
+	
+  GPIO_InitStructure.GPIO_Pin =  PLAY_BUTTON_PIN;                  //LED Pin
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;            //Push pull output
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;        //100MHz
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+  GPIO_Init(PLAY_BUTTON_PORT, &GPIO_InitStructure);                    //Initialize LED GPIO
+	GPIO_SetBits(PLAY_BUTTON_PORT,PLAY_BUTTON_PIN);
+}
+
+void pbutton_task(void *pvParameters)
+{		
+	
+    while(1)
+    {
+    }
+}  
+*/
+
+/*
+if(PLAY_BUTTON)
+		BUZ=~BUZ;
+  currently when pressed the button will actually be 0
+  not pressed = 1
+*/
+
+
+
+
